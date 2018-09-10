21d9b037e83479b04ff90fbecd7686c2e6011cf1
Alymostafa2000 <34445689+Alymostafa@users.noreply.github.com>
2018-09-10 12:20:52 +0300

    Update first Neural Network using tensorflow(keras).py

--- first Neural Network using tensorflow(keras).py
+++ first Neural Network using tensorflow(keras).py
@@ -1,106 +1,44 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+#helpier libraries
 # In[1]:
-
-
 import tensorflow as tf
-
-
 # In[4]:
-
-
 from tensorflow import keras as k
-
-
 # In[5]:
-
-
 import numpy as np
-
-
 # In[6]:
-
-
 import matplotlib.pyplot as plt
-
-
+#helpier libraries
 # In[8]:
-
-
-
-
-print(tf.__version__)
-
-
+print(tf.__version__)#to know the version of your tf
 # In[12]:
-
-
-fashion_mnist=keras.datasets.fashion_mnist
+fashion_mnist=keras.datasets.fashion_mnist #load your dataset
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-
 # In[14]:
-
-
+#give names to your dataset
 class_names=['t-shirt/top','trouser','pullover','dress','coat','sandal','shirt','sneaker','bag','ankleboot']
-
-train_images.shape
+train_images.shape 
 # In[15]:
-
-
-train_images.shape
-
-
 # In[16]:
-
-
 test_images.shape
-
-
 # In[17]:
-
-
 len(train_labels)
-
-
 # In[18]:
-
-
 train_labels.shape
-
-
 # In[19]:
-
-
 train_labels
-
-
 # In[20]:
-
-
 test_labels.shape
-
-
 # In[24]:
-
-
 #plt.figure()
 plt.imshow(train_images[0])
 #plt.colorbar()
 #plt.grid(False)
-
-
 # In[25]:
-
-
-train_images=train_images/255.0
+train_images=train_images/255.0 #to make it 0,1
 test_images=test_images/255.0
-
-
 # In[54]:
-
-
 plt.figure(figsize=(10,10))
 for i in range(25):
     plt.subplot(6,6,i+1)
@@ -110,68 +48,30 @@ for i in range(25):
     plt.imshow(train_images[i])
     plt.xlabel(class_names[train_labels[i]])
     #plt.ylabel(class_names[train_labels[i]])
-
-
-# In[86]:
-
-
-model=keras.Sequential([
+# In[86]: #make layers to your NN
+model=keras.Sequential([ 
     keras.layers.Flatten(input_shape=(28,28)),
     keras.layers.Dense(128,activation=tf.nn.relu),
     keras.layers.Dense(10,activation=tf.nn.softmax)
 ])
-
-
-#  
-
 # In[87]:
-
-
 model.compile(optimizer=tf.train.AdamOptimizer()
               ,loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-
-
 # In[89]:
-
-
-model.fit(train_images, train_labels, epochs=5)
-
-
+model.fit(train_images, train_labels, epochs=5) #you can increase epochs to increase accu
 # In[93]:
-
-
 test_loss,test_acc=model.evaluate(test_images,test_labels)
 print('test accuracy',test_acc)
-
-
 # In[94]:
-
-
 predictions=model.predict(test_images)
-
-
 # In[95]:
-
-
 predictions[0]
-
-
 # In[96]:
-
-
 np.argmax(predictions[0])
-
-
 # In[97]:
-
-
 test_labels[0]
-
-
 # In[98]:
-
-
 def plot_image(i, predictions_array, true_label, img):
   predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
   plt.grid(False)
@@ -203,21 +103,14 @@ def plot_value_array(i, predictions_array, true_label):
   thisplot[predicted_label].set_color('red')
   thisplot[true_label].set_color('blue')
 
-
 # In[99]:
-
-
 i = 0
 plt.figure(figsize=(6,3))
 plt.subplot(1,2,1)
 plot_image(i, predictions, test_labels, test_images)
 plt.subplot(1,2,2)
 plot_value_array(i, predictions,  test_labels)
-
-
 # In[102]:
-
-
 i = 25
 plt.figure(figsize=(6,3))
 plt.subplot(1,2,1)
@@ -225,9 +118,7 @@ plot_image(i, predictions, test_labels, test_images)
 plt.subplot(1,2,2)
 plot_value_array(i, predictions,  test_labels)
 
-
 # In[103]:
-
 
 # Plot the first X test images, their predicted label, and the true label
 # Color correct predictions in blue, incorrect predictions in red
@@ -241,42 +132,30 @@ for i in range(num_images):
   plt.subplot(num_rows, 2*num_cols, 2*i+2)
   plot_value_array(i, predictions, test_labels)
 
-
 # In[104]:
-
 
 img=test_images[0]
 print(img.shape)
 
-
 # In[105]:
-
 
 img = (np.expand_dims(img,0))
 print(img.shape)
 
-
 # In[107]:
-
 
 prediction_single=model.predict(img)
 print(prediction_single)
 
-
 # In[116]:
-
 
 plot_value_array(0, prediction_single, test_labels)
 _ = plt.xticks(range(10), class_names,rotation=30)
 
-
 # In[117]:
-
 
 np.argmax(prediction_single[0])
 
-
-# In[ ]:
 
 
 
